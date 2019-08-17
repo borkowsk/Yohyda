20ddc7450ed776069d7b06896c1650f14747d9c7
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-08-17 23:10:16 +0200

    Usuwanie oprawki HTML

--- serverside/main.cpp
+++ serverside/main.cpp
@@ -1,51 +1,3 @@
-/*<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>doc/html/boost_asio/example/cpp11/http/server/main.cpp - 1.62.0</title>  <link rel="icon" href="/favicon.ico" type="image/ico" />
-  <link rel="stylesheet" type="text/css" href="/style-v2/section-doc.css" />
-  <!--[if IE 7]> <style type="text/css"> body { behavior: url(/style-v2/csshover3.htc); } </style> <![endif]-->
-
-</head>
-
-<body>
-  <div id="heading">
-    <div class="heading-inner">
-  <div class="heading-placard"></div>
-
-  <h1 class="heading-title">
-  <a href="/">
-  <img src="/gfx/space.png" alt= "Boost C++ Libraries" class="heading-logo" />
-  <span class="heading-boost">Boost</span>
-  <span class="heading-cpplibraries">C++ Libraries</span>
-  </a></h1>
-
-  <p class="heading-quote">
-  <q>...one of the most highly
-  regarded and expertly designed C++ library projects in the
-  world.</q> <span class="heading-attribution">&mdash; <a href=
-  "http://www.gotw.ca/" class="external">Herb Sutter</a> and <a href=
-  "http://en.wikipedia.org/wiki/Andrei_Alexandrescu" class="external">Andrei
-  Alexandrescu</a>, <a href=
-  "http://safari.awprofessional.com/?XmlId=0321113586" class="external">C++
-  Coding Standards</a></span></p>
-</div>
-  </div>
-  <div class="boost-common-header-notice"><a class="boost-common-header-inner" href="/doc/libs/release/doc/html/boost_asio/example/cpp11/http/server/main.cpp">This is the documentation for an old version of Boost.
-                Click here to view this page for the latest version.</a></div>
-
-  <div id="body">
-    <div id="body-inner">
-      <div id="content">
-        <div class="section" id="docs">
-          <div class="section-0">
-            <div class="section-body">
-              <h3>doc/html/boost_asio/example/cpp11/http/server/main.cpp</h3>
-<pre>
-*/
 //
 // main.cpp
 // ~~~~~~~~
@@ -53,13 +5,13 @@
 // Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at <a href="http://www.boost.org/LICENSE_1_0.txt">http://www.boost.org/LICENSE_1_0.txt</a>)
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include &lt;iostream&gt;
-#include &lt;string&gt;
-#include &lt;<a href="../../../../../../../boost/asio.hpp">boost/asio.hpp</a>&gt;
-#include &quot;server.hpp&quot;
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include "server.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -68,11 +20,11 @@ int main(int argc, char* argv[])
     // Check command line arguments.
     if (argc != 4)
     {
-      std::cerr &lt;&lt; &quot;Usage: http_server &lt;address&gt; &lt;port&gt; &lt;doc_root&gt;\n&quot;;
-      std::cerr &lt;&lt; &quot;  For IPv4, try:\n&quot;;
-      std::cerr &lt;&lt; &quot;    receiver 0.0.0.0 80 .\n&quot;;
-      std::cerr &lt;&lt; &quot;  For IPv6, try:\n&quot;;
-      std::cerr &lt;&lt; &quot;    receiver 0::0 80 .\n&quot;;
+      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+      std::cerr << "  For IPv4, try:\n";
+      std::cerr << "    receiver 0.0.0.0 80 .\n";
+      std::cerr << "  For IPv6, try:\n";
+      std::cerr << "    receiver 0::0 80 .\n";
       return 1;
     }
 
@@ -82,13 +34,14 @@ int main(int argc, char* argv[])
     // Run the server until stopped.
     s.run();
   }
-  catch (std::exception&amp; e)
+  catch (std::exception& e)
   {
-    std::cerr &lt;&lt; &quot;exception: &quot; &lt;&lt; e.what() &lt;&lt; &quot;\n&quot;;
+    std::cerr << "exception: " << e.what() << "\n";
   }
 
   return 0;
 }
+
 /*
 </pre>
             </div>
