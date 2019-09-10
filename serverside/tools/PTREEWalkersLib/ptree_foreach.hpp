fdcf3b64991aa922af15ab48ce67bc4d19c7e20d
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-09-10 21:17:58 +0200

    Skorygowane  foreach_node i pokrewne

--- serverside/tools/PTREEWalkersLib/ptree_foreach.hpp
+++ serverside/tools/PTREEWalkersLib/ptree_foreach.hpp
@@ -21,17 +21,40 @@ using boost::property_tree::ptree;
 /// Typ predykatowy:
 /// Ma do dyspozycji pełną ścieżkę do aktualnego węzła/liścia std::string key/k
 /// oraz sam węzeł ptree& tree/t
-/// No więc i dane węzła tree.data() i wszystko co poniżej.
+/// No więc i dane węzła tree.data() i jak konieczne to wszystko co poniżej.
 
 using pred  = const std::function<bool (ptree& tree,std::string key)>;//Typ predykatu
 using predc = const std::function<bool (const ptree& tree,std::string key)>;//Typ predykatu dla stałego ptree
 
-inline bool  never(const ptree& t,std::string k){ return true; }//Predykat "zawsze nie"
-inline bool always(const ptree& t,std::string k){ return true; }//Predykat "zawsze tak"
+/// Użyteczne predykaty
+inline bool  never(const ptree& t,std::string k){ return false; }//Predykat "zawsze nie"
+inline bool always(const ptree& t,std::string k){ return true;  }//Predykat "zawsze tak"
+inline bool  print(const ptree& t,std::string k){ //Drukuj i zwracaj true - Pomocny predykat testowy
+                                                 std::cout<< k <<":\t";
+                                                 std::cout<< t.data() <<" ; "<<std::endl;
+                                                 return true; }
 
-///Funkcja odwiedza wszytkie wezly i liscie drzewa ptree
-/// UWAGA NA MODYFIKACJE DRZEWA!!!
-/// W miarę bezpiecznie można to zrobić tylko w funkcji „after”
+
+/// Funkcja ODWIEDZAJĄCA wszystkie wezly i liscie drzewa ptree
+/// Przechodzi wszystkie elementy, wykonując "filter", ale
+///     jeśli "filter" zwróci "true" to
+///          wykonuje "before"
+///             potem schodzi rekurencyjnie, a po powrocie z rekurencji
+///               jeśli "before" zwróciło "true" to
+///                      wykonuje "after"
+///
+inline
+void foreach_node(const ptree &tree,//Korzen drzewa w wersji const!
+                    std::string key,//Sciezka do węzła - niekonieczna niby, ale zwykle przydatna
+                    predc& filter,   //Predykat wykonywany zawsze
+                    predc& before=never,//Predykat wykonywany gdy pierwszy zwróci true - przed iteracją dzieci
+                    predc& after=never);//Predykat wykonywany gdy poprzedni zwróci true - po iteracji dzieci
+
+/// Tu są możliwe zmiany w drzewie, ale...
+/// UWAGA NA MODYFIKACJE!!!
+/// W miarę bezpiecznie można to zrobić tylko w funkcji „after”,
+/// w funkcji "filter" i "before" tylko pod warunkiem  
+/// starannego posprzątania po sobie! Zwłaszcza uważac na usuwanie.
 ///
 inline
 void foreach_node(ptree &tree,      //Korzen drzewa
@@ -40,41 +63,25 @@ void foreach_node(ptree &tree,      //Korzen drzewa
                     pred& before=never,//Predykat wykonywany gdy pierwszy zwróci true - przed iteracją dzieci
                     pred& after=never);//Predykat wykonywany gdy poprzedni zwróci true - po iteracji dzieci
 
-inline
-void foreach_node(const ptree &tree,//Korzen drzewa w wersji const!
-                    std::string key,//Sciezka do węzła - niekonieczna niby ale zwykle przydatna
-                    predc& filter,   //Predykat wykonywany zawsze
-                    predc& before=never,//Predykat wykonywany gdy pierwszy zwróci true - przed iteracją dzieci
-                    predc& after=never);//Predykat wykonywany gdy poprzedni zwróci true - po iteracji dzieci
-
-///Pomocny predykat testowy
-inline bool  print(const ptree& t,std::string k){ //Drukuj i zwracaj true
-                                 std::cout<< k <<":\t";
-                                 std::cout<< t.data() <<" ; "<<std::endl;
-                                 return true; }
-
 /// Numeracja nienazwanych "dzieci"
-inline void   insert_numbers(ptree& pt);//numeruje wszystkie nienazwane dzieci każdegowęzła
+/// Funkcja nadaje kolejne(?) numery wszystkim nienazwanym 
+/// dzieciom każdego węzła
+///
+inline void   insert_numbers(ptree& pt);
 
 /// OBSŁUGA "LINKU SYMBOLICZNEGO" DO RODZICA
 inline void   insert_ups(ptree& pt);//wstawia do ptree symboliczne linki do rodziców
 inline void   delete_ups(ptree& pt);//usuwa z ptree symboliczne linki do rodziców
 inline ptree* get_parent(ptree& pt);//daje rodzica danego wezla ptree
-                            //Odczytuje i konwertuje
+                                    //Odczytuje i konwertuje
 
-/// Funkcja użytkowa - drukowanie wszystkiego na stdout
-inline void print_all(const ptree& pt); //Drukuje wszystko
+/// Funkcja użytkowa - drukowanie wszystkiego na strumień
+inline void print_all_to(const ptree& pt,std::ostream& out=std::cout); //Drukuje wszystko
 
 //IMPLEMENTACJA:
-/// foreach_node(...) to ogólny algorytm robienia czegoś z elementami ptree
-/// (Dobry do lambd, ale jeszcze nie jako template)
-/// Przechodzi wszystkie elementy, wykonując "filter", ale
-///     jeśli "filter" zwróci "true" to
-///          wykonuje "before"
-///             potem schodzi rekurencyjnie, a po powrocie z rekurencji
-///               jeśli "before" zwróciło "true" to
-///                      wykonuje "after"
-
+//=====================
+// foreach_node(...) to ogólny algorytm robienia czegoś z drzewami ptree
+// (Dobry do lambd, ale jeszcze nie jako template)
 void foreach_node(const ptree &tree, std::string key, predc& filter, predc& before/*=never*/, predc& after/*=never*/)
 {
     std::string nkey;
@@ -82,22 +89,23 @@ void foreach_node(const ptree &tree, std::string key, predc& filter, predc& befo
 
     if (!key.empty())
     {
-      nkey = key + ".";  // separator!!! TODO?
+        nkey = key + ".";  // separator!!! TODO?
     }
 
-    if(filter(tree,key))
-        do_after=before(tree,key);
+    if(filter(tree,key))// FILTR ZAWSZE
+        do_after=before(tree,key);// Jak filtr OK czyli 'true'
 
-    auto end = tree.end();
+    auto end = tree.end();// REKURENCJA TEŻ ZAWSZE
     for (auto it = tree.begin(); it != end; ++it)
     {
-      foreach_node(it->second, nkey + it->first ,filter,before,after);
+        foreach_node(it->second, nkey + it->first ,filter,before,after);
     }
 
-    if(do_after)
+    if(do_after)// Jak before zwróciło "true"
         after(tree,key);
 }
 
+// Wersja foreach_node z możliwością modyfikacji
 void foreach_node(ptree &tree, std::string key, pred& filter, pred& before/*=never*/, pred& after/*=never*/)
 {
     std::string nkey;
@@ -105,7 +113,7 @@ void foreach_node(ptree &tree, std::string key, pred& filter, pred& before/*=nev
 
     if (!key.empty())
     {
-      nkey = key + ".";  // separator!!! TODO?
+        nkey = key + ".";
     }
 
     if(filter(tree,key))
@@ -114,48 +122,52 @@ void foreach_node(ptree &tree, std::string key, pred& filter, pred& before/*=nev
     auto end = tree.end();
     for (auto it = tree.begin(); it != end; ++it)
     {
-      foreach_node(it->second, nkey + it->first ,filter,before,after);
+        foreach_node(it->second, nkey + it->first ,filter,before,after);
     }
 
     if(do_after)
         after(tree,key);
 }
 
-//Brak nazw węzłów pochodzącyz z tablic jest bardzo
-//niewygodny, gdy trzeba się do nich dostać pojedynczo.
-//Funkcja numeruje wszystkie nienazwane dzieci każdego węzła
+// Brak nazw węzłów pochodzących z tablic json'a jest bardzo
+// niewygodny, gdy trzeba się do nich dostać pojedynczo.
+//
 void insert_numbers(ptree& pt)
+// Funkcja numeruje wszystkie nienazwane dzieci każdego węzła
 {
-    fasada::foreach_node(pt,"",
-    [   ](ptree& t,std::string k)
-    {
-        unsigned id=1;//Trzeba nadać id węzłom o pustych nazwach?
-
-        while(1)
+    foreach_node(pt,"",
+        [](ptree& t,std::string k)
         {
-            auto fp=t.find("");
-            if(fp==t.not_found()) break;
-            ptree::iterator it = t.to_iterator(fp);
-            auto newname=boost::lexical_cast< std::string >(id++);
-            t.insert(it, make_pair(newname, it->second));
-            t.erase(it);
+            unsigned id=1;//Trzeba nadać id węzłom o pustych nazwach?
+
+            while(1)
+            {
+                //base on https://stackoverflow.com/questions/45262602/c-boost-ptree-rename-key
+                auto fp=t.find("");
+                if(fp==t.not_found()) break;
+                ptree::iterator it = t.to_iterator(fp);
+                auto newname=boost::lexical_cast< std::string >(id++);
+                t.insert(it, make_pair(newname, it->second));
+                t.erase(it);
+            }
+            //return id>1?true:false;//DEBUG
+            return true;
         }
-        return id>1?true:false;
-    },print
     );
 }
-//Potrzebujemy prostej metody znajdowania rodzica wezla.
-//Niestety ptree tego nie przewiduje:
+
+// Potrzebujemy prostej metody znajdowania rodzica wezla.
+// Niestety ptree tego nie przewiduje:
 // https://stackoverflow.com/questions/45366768/getting-boost-property-tree-parent-node
 // https://stackoverflow.com/questions/45255463/c-boost-ptree-relative-key
-//Pomysl jest taki zeby do listy dzieci wezla wstawiac wezel rodzica, tak jak to sie
-//robi w strukturze katalogow filesystemu ("..")
-//Ale raczej to nie moze byc normalny wskaznik   do node bo sie drzewo "scyklizuje"(?)
+// Pomysl jest taki zeby do listy dzieci wezla wstawiac wezel rodzica, tak jak to sie
+// robi w strukturze katalogow filesystemu ("..")
+// Ale raczej to nie może być normalny wskaźnik do 'node' bo się drzewo "scyklizuje"(?)
 //
 void insert_ups(ptree& pt)
 //wstawia symboliczne linki do rodziców
 {
-    std::stack<ptree*> pointers;//Jak sensownie zachować adresy rodzica? stack? TODO!!!
+    std::stack<ptree*> pointers;//Jak sensownie zachować adresy rodzica? stack? TODO?
     pointers.push(nullptr); //Root tez cos musi sobie wstawic.
                             //NULL symbolizuje brak wyzszej instancji
 
@@ -190,6 +202,7 @@ void delete_ups(ptree& pt)
 }
 
 ptree* get_parent(ptree& pt)
+//daje "zdeszyfrowany" link do rodzica
 {
     boost::optional<ptree&> uplink=pt.get_child_optional("^");
     if(uplink)
@@ -205,35 +218,38 @@ ptree* get_parent(ptree& pt)
         result = strtoull(sptr.c_str(), &eptr, 16);
 
         if (result == 0)//Tymczasowa obsluga bledow
-          {
-              /* If a conversion error occurred, display a message and exit */
-              if (errno == EINVAL)
-              {
-                  std::cerr<<"Conversion error occurred: "<< errno <<std::endl;
-              }
+        {
+            /* If a conversion error occurred, display a message and exit */
+            if (errno == EINVAL)
+            {
+              std::cerr<<"Conversion error occurred: "<< errno <<std::endl;
+            }
 
-              /* If the value provided was out of range, display a warning message */
-              if (errno == ERANGE)
-                  std::cerr<<"The value provided was out of range"<< errno <<std::endl;
+            /* If the value provided was out of range, display a warning message */
+            if (errno == ERANGE)
+              std::cerr<<"The value provided was out of range"<< errno <<std::endl;
 
-             return nullptr;
-          }
+            return nullptr;
+         }
 
 
-        return (ptree*)result;//WIEMY ZE TO JEEEST TOOO!
+        return (ptree*)result;//WIEMY ZE TO JEEEST TO!
     }
     else
     return nullptr;
 }
 
-void print_all(const ptree& pt)
+void print_all_to(const ptree& pt,std::ostream& out/*=std::cout*/)
+// drukuje wszystko na zadany strumień
 {
-    foreach_node(pt,"root",always,[](const ptree& t,std::string k)
-    {
-        std::cout<< k <<":\t";
-        std::cout<< t.data() <<" ; "<<std::endl;
-        return false;//blokuje wywołanie "after"
-    });
+    foreach_node(pt,"root",always,
+        [&out](const ptree& t,std::string k)
+        {
+            out<< k <<":\t";
+            out<< t.data() <<" ; "<<std::endl;
+            return false;//blokuje wywołanie "after", które i tak jest "never"
+        }
+        );
 }
 
 }//namespace "fasada"
