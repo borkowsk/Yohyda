cc61cb8fb46e4dc818b431d723a9698a434ceca1
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-09-18 17:46:36 +0200

    Headers adopted for use with Fasada-2019

--- serverside/wwwserver1/reply.hpp
+++ serverside/wwwserver1/reply.hpp
@@ -7,6 +7,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+// Adopted for FASADA (c) 2019 by Wojciech Borkowski
+// (wborkowsk MAŁPA gmail KROPA com , wborkowski MAŁPA uw KROPA edu KROPA pl)
+//
 
 #ifndef HTTP_REPLY_HPP
 #define HTTP_REPLY_HPP
