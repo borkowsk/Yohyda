a5b22d8e9980c953c19a7a4c92c24d5883054f63
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-10-27 00:41:45 +0200

    Useable versions of ren-ame & del-ate processors

--- serverside/fasada/processor_del.cpp
+++ serverside/fasada/processor_del.cpp
@@ -1,9 +1,20 @@
+#include "tree_types.h"
+#include "tree_consts.h"
 #include "processor_del.h"
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string/replace.hpp> ///https://stackoverflow.com/questions/4643512/replace-substring-with-another-substring-c
 
 namespace fasada
 {
+
+//default HTML form for this processor
+std::string processor_del::Form=
+        "<form action=\"$fullpath!$proc\" class=\"fasada_form\">\n"
+        "NAME: "
+        "<input type=\"text\" name=\"name\" size=\"" STR_DEFAULT_LEN_OF_NAME "\"><br>\n"
+        "will be DELeted from <B class=fasada_path>'$path'</B><BR>\n"
+        "<input type=\"submit\" value=\"OK\">\n"
+        "</form>";
 
 processor_del::processor_del(const char* name):
     tree_processor(WRITER_READER,name) //also READER because is able to create its own FORM
@@ -12,5 +23,76 @@ processor_del::processor_del(const char* name):
 
 processor_del::~processor_del()
 {}
+
+
+void processor_del::_implement_read(ShmString& o,const pt::ptree& top,URLparser& request)
+//Implement_read WRITER'a powinno przygotować FORM jeśli jest to format "html"
+{
+    std::string fullpath=request.getFullPath();
+    std::string tmp=top.get_value<std::string>();
+    unsigned    noc=top.size();//czy ma elementy składowe?
+    bool html=request["html"]!="false";
+
+    if(html)//TYPE HEADER AND HTML HEADER
+    {
+         o+=ipc::string(EXT_PRE)+"htm\n";
+         o+=getHtmlHeaderDefaults(fullpath)+"\n";
+         if(top.data()=="" && noc>0)
+         {
+             //Podmienić ścieżkę i wartość domyślną
+             std::string ReadyForm=Form;
+             boost::replace_all(ReadyForm,"$proc",procName);
+             boost::replace_all(ReadyForm,"$fullpath",fullpath);
+             boost::replace_all(ReadyForm,"$path",request["&path"]);
+             o+=ReadyForm;
+         }
+         else
+         {
+             o+="<H2>WARNING!</H2><P>Only not-leaf type nodes could be modified by '"+procName+"'</P>";
+         }
+         o+=getHtmlClosure();
+    }
+    else
+    {
+        o+=ipc::string(EXT_PRE)+"txt\n";
+        o+="FOR DELETE USE: "+fullpath+"?!"+procName+"&name=[NAME]";
+    }
+}
+
+void processor_del::_implement_write(ShmString& o,pt::ptree& top,URLparser& request)
+//Implement_write WRITER'a powinno zmienić wartości na powstawie FORMularza z method==GET
+{
+    std::string fullpath;
+
+    if(top.data()!="")//Jeśli ma wartość własną to jest liściem
+        throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT DELETE CHILD FROM LEAF NODE!"));
+
+    std::string name=request["name"];
+    if(name=="")
+        throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT DELETE NONAME CHILD"));
+
+    bool html=request["html"]!="false";
+
+    if(html)
+    {
+        fullpath=request.getFullPath();
+        o+=ipc::string(EXT_PRE)+"htm\n";
+        o+=getHtmlHeaderDefaults(fullpath)+"\n<P>";
+    }
+    else
+        o+=ipc::string(EXT_PRE)+"txt\n";
+
+    //top.put(name,request["value"]);//Not duplicate names.
+    top.erase(name);
+
+    if(html)
+    {
+       o+="DONE <B class=fasada_path>'"+request["&path"]+"/"+name+"'</B> deleted<BR>\n";
+       o+="\n"+getActionLink(fullpath+"?ls&long&html",HTMLBack);
+       o+="</P>";
+       o+=getHtmlClosure();
+    }
+    else o+="DONE '"+name+"'='"+top.get_child(name).data()+"'";
+}
 
 }//namespace "fasada"
--- serverside/fasada/processor_ren.cpp
+++ serverside/fasada/processor_ren.cpp
@@ -1,9 +1,25 @@
+#include "tree_types.h"
+#include "tree_consts.h"
 #include "processor_ren.h"
+//include <boost/process/io.hpp>
+#include <boost/property_tree/ptree.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string/replace.hpp> ///https://stackoverflow.com/questions/4643512/replace-substring-with-another-substring-c
 
 namespace fasada
 {
+
+//default HTML form for this processor
+std::string processor_ren::Form=
+        "<form action=\"$fullpath!$proc\" class=\"fasada_form\">\n"
+        "OLD NAME: "
+        "<input type=\"text\" name=\"old_name\" size=\"" STR_DEFAULT_LEN_OF_NAME "\"><br>\n"
+        "NEW NAME: "
+        "<input type=\"text\" name=\"new_name\" size=\"" STR_DEFAULT_LEN_OF_NAME "\"><br>\n"
+        "will be RENamed in <B class=fasada_path>'$path'</B> <br>"
+        "<input type=\"submit\" value=\"OK\">"
+        "</form>";
+
 
 processor_ren::processor_ren(const char* name):
     tree_processor(WRITER_READER,name) //also READER because is able to create its own FORM
@@ -12,5 +28,90 @@ processor_ren::processor_ren(const char* name):
 
 processor_ren::~processor_ren()
 {}
+
+void processor_ren::_implement_read(ShmString& o,const pt::ptree& top,URLparser& request)
+//Implement_read WRITER'a powinno przygotować FORM jeśli jest to format "html"
+{
+    std::string fullpath=request.getFullPath();
+    std::string tmp=top.get_value<std::string>();
+    unsigned    noc=top.size();//czy ma elementy składowe?
+    bool html=request["html"]!="false";
+
+    if(html)//TYPE HEADER AND HTML HEADER
+    {
+         o+=ipc::string(EXT_PRE)+"htm\n";
+         o+=getHtmlHeaderDefaults(fullpath)+"\n";
+         if(top.data()=="")
+         {
+             //Podmienić ścieżkę i wartość domyślną
+             std::string ReadyForm=Form;
+             boost::replace_all(ReadyForm,"$proc",procName);
+             boost::replace_all(ReadyForm,"$fullpath",fullpath);
+             boost::replace_all(ReadyForm,"$path",request["&path"]);
+             o+=ReadyForm;
+         }
+         else
+         {
+             o+="<H2>WARNING!</H2><P>Only not-leaf type nodes could be modified by '"+procName+"'</P>";
+         }
+         o+=getHtmlClosure();
+    }
+    else
+    {
+        o+=ipc::string(EXT_PRE)+"txt\n";
+        o+="FOR ren USE: "+fullpath+"?!"+procName+"&name=[NEW_NAME]&value=[NEW_VALUE]";
+    }
+}
+
+using boost::property_tree::ptree;
+
+void processor_ren::_implement_write(ShmString& o,pt::ptree& top,URLparser& request)
+//Implement_write WRITER'a powinno zmienić wartości na powstawie FORMularza z method==GET
+{
+
+    std::string fullpath;
+
+    std::string old_name=request["old_name"];
+    if(old_name=="")
+        throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT RENAME CHILD WITHOUT NAME!"));
+
+    std::string new_name=request["new_name"];
+    if(new_name=="")
+        throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT RENAME CHILD INTO EMPTY NAME!"));
+
+    bool html=request["html"]!="false";
+
+    if(html)
+    {
+        fullpath=request.getFullPath();
+        o+=ipc::string(EXT_PRE)+"htm\n";
+        o+=getHtmlHeaderDefaults(fullpath)+"\n<P>";
+    }
+    else
+        o+=ipc::string(EXT_PRE)+"txt\n";
+
+    //top.ren_child(old_name,new_name);//NIE MA CZEGOŚ TAKIEGO
+    //ptree::iterator
+    ptree::iterator ne = top.to_iterator(top.find(new_name));
+    //if(ne!=top.not_found())
+      //  throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT RENAME INTO EXISTING '"+new_name+"'!"));
+
+    ptree::iterator it = top.to_iterator(top.find(old_name));
+    //if(it==top.not_found())
+      //  throw(tree_processor_exception("PTREE PROCESSOR '"+procName+"' CANNOT FIND CHILD WITH NAME '"+old_name+"'!"));
+
+    top.insert(it, make_pair(new_name, it->second));
+    top.erase(it);
+
+    if(html)
+    {
+       o+="DONE <B class=fasada_path>'"+old_name+"'</B>--><B>'"+new_name+"'</B>";
+       o+="\n"+getActionLink(fullpath+"?ls&long&html",HTMLBack);
+       o+="</P>";
+       o+=getHtmlClosure();
+    }
+    else o+="DONE '"+old_name+"'-->'"+new_name+"'";
+}
+
 
 }//namespace "fasada"
