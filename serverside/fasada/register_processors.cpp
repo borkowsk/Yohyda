58095dc0aaea7a2bcdf5594e73bc42fdf78c9a24
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-10-27 23:35:01 +0100

    Registration of processor_find & some aliases added

--- serverside/fasada/register_processors.cpp
+++ serverside/fasada/register_processors.cpp
@@ -6,22 +6,25 @@
 #include "processor_ren.h"
 #include "processor_ls.h"
 #include "processor_dfs.h"
+#include "processor_find.h"
 
 namespace fasada
 {
 
 void register_processors()
 {
-    static fasada::processor_get GET;//Istnieje conajmniej jedna zmienna kazdego typu
-    static fasada::processor_set SET;//--//---
-    static fasada::processor_add ADD;//--//---
-    static fasada::processor_del DEL;//--//---
-    static fasada::processor_ren REN;//--//---
-    static fasada::processor_dfs DFS;//--//---
-    static fasada::processor_ls  LS; //--//---
+    static fasada::processor_get  GET;//Istnieje co najmniej jedna zmienna kazdego typu
+    static fasada::processor_set  SET;//--//---
+    static fasada::processor_add  ADD;//--//---
+    static fasada::processor_del  DEL;//--//---
+    static fasada::processor_ren  REN;//--//---
+    static fasada::processor_dfs  DFS;//--//---
+    static fasada::processor_ls   LST; //--//---
+    static fasada::processor_find FIND;//--//---
 
     static fasada::processor_ls  Def("default");//ale mozna robić aliasy
     static fasada::processor_ls  Dir("dir");
+    static fasada::processor_ls  Lst("lst");
     static fasada::processor_dfs Tree("tree");
 }
 
