c17d7a78ed71334e1ff9bc82212cadcde569518b
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-10-27 23:33:53 +0100

    processor_find added to fasada library

--- serverside/fasada/processor_find.cpp
+++ serverside/fasada/processor_find.cpp
@@ -1,6 +1,28 @@
+#include "tree_types.h"
+#include "tree_consts.h"
 #include "processor_find.h"
+#include "tree/ptree_foreach.hpp"
+#include <iostream>
 
-processor_find::processor_find()
+namespace fasada
 {
 
+processor_find::processor_find(const char* name):
+    tree_processor(READER,name)
+{}
+
+processor_find::~processor_find()
+{}
+
+void processor_find::_implement_write(ShmString& o,pt::ptree& top,URLparser& request)
+{
+    throw(tree_processor_exception("PTREE PROCESSOR "+procName+"IS REALLY NOT A WRITER!"));
 }
+
+void processor_find::_implement_read(ShmString& o,const pt::ptree& top,URLparser& request)
+{
+    throw(tree_processor_exception("PTREE PROCESSOR "+procName+"IS NOT IMPLEMENTED YET!"));
+}
+
+
+}//namespace "fasada"
