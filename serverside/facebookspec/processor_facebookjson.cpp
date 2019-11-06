4fe3b6c042fa0c794a011edb248da230228461ce
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-11-06 20:20:51 +0100

    Processor FacebookJson for lading facebook backup jsons initiated

--- serverside/facebookspec/processor_facebookjson.cpp
+++ serverside/facebookspec/processor_facebookjson.cpp
@@ -1,6 +1,43 @@
 #include "processor_facebookjson.h"
 
-processor_facebookJson::processor_facebookJson()
+namespace facebook
 {
 
+//default HTML form for this processor
+std::string processor_facebookJson::Form="";
+//        "<form action=\"$fullpath!$proc\" class=\"fasada_form\">\n"
+//        "FOR <B class=fasada_path>'$path'</B><BR>\n"
+//        "<input type=\"submit\" value=\"OK\">\n"
+//        "</form>";
+
+processor_facebookJson::processor_facebookJson(const char* name):
+    tree_processor(WRITER,name) //also READER because is able to create its own FORM
+{
 }
+
+processor_facebookJson::~processor_facebookJson()
+{}
+
+
+void processor_facebookJson::_implement_read(ShmString& o,const pt::ptree& top,URLparser& request)
+{
+    throw(tree_processor_exception("READ FUNCTION FOR PTREE PROCESSOR "+procName+" NOT IMPLEMENTED!"));
+}
+
+void processor_facebookJson::_implement_write(ShmString& o,pt::ptree& top,URLparser& request)
+//Implement_write WRITER'a powinno zmienić wartości na powstawie FORMularza z method==GET
+{
+    std::string fullpath;
+    unsigned    noc=top.size();//czy ma elementy składowe?
+
+    if(noc!=0)
+    {
+        //o+="Only leaf type nodes can be modified by "+procName+"\n";
+        throw(tree_processor_exception("PTREE PROCESSOR "+procName+" CANNOT LOAD NOT-LEAF NODE!"));
+    }
+
+    //....
+
+}
+
+}//namespace Facebook
--- serverside/facebookspec/processor_facebookjson.h
+++ serverside/facebookspec/processor_facebookjson.h
@@ -1,11 +1,31 @@
 #ifndef PROCESSOR_FACEBOOKJSON_H
 #define PROCESSOR_FACEBOOKJSON_H
 
+#if defined(BOOST_HAS_PRAGMA_ONCE)
+#  pragma once
+#endif
+#include "tree_processor.h"
 
-class processor_facebookJson : public tree_processor
+using namespace fasada;
+
+namespace facebook
 {
+
+class processor_facebookJson : public tree_processor //WRITER
+{
+protected:
+    static std::string Form;//default HTML form for this processor
+
 public:
-    processor_facebookJson();
+    processor_facebookJson(const char* name="FacebookJson");
+   ~processor_facebookJson();
+ protected://implementation
+ virtual
+     void _implement_read(ShmString& o,const pt::ptree& top,URLparser& request);
+ virtual
+     void _implement_write(ShmString& o,pt::ptree& top,URLparser& request);
 };
+
+} //namespace "facebook"
 
 #endif // PROCESSOR_FACEBOOKJSON_H
