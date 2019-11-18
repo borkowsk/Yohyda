25dcbef534539d590e2693efb46e3ed2bf0384ff
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-11-18 12:55:46 +0100

    New class processor_check_file invented

---
+++ serverside/fasada/processor_check_file.cpp
@@ -0,0 +1,6 @@
+#include "processor_check_file.h"
+
+processor_check_file::processor_check_file()
+{
+
+}
---
+++ serverside/fasada/processor_check_file.h
@@ -0,0 +1,11 @@
+#ifndef PROCESSOR_CHECK_FILE_H
+#define PROCESSOR_CHECK_FILE_H
+
+
+class processor_check_file : public tree_processor
+{
+public:
+    processor_check_file();
+};
+
+#endif // PROCESSOR_CHECK_FILE_H
