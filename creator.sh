d93042c9048a99460821314d2608985ff8fbaa16
Wojciech Borkowski <wborkowsk@gmail.com>
2019-11-21 14:48:41 +0100

    Creator.sh (PL) added

---
+++ creator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "KREATOR INSTALACJI SYSTEMU FASADA DLA UBUNTU (polskojęzyczny)"
+echo "(For english version use creator_en.sh)"
+echo "..."
+echo "NIE MAM JUŻ NIC WIĘCEJ DO ZROBIENIA"
