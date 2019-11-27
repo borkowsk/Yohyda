202e73af79d64db88bea5f334e545253cba715a7
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-11-27 19:17:44 +0100

    Twitter loader uncommented & some code cosmetics provided

--- fasada-core/lib/fasada/register_processors.cpp
+++ fasada-core/lib/fasada/register_processors.cpp
@@ -1,4 +1,4 @@
-//Procesory danych json w postaci zmiennych w main.
+//Procesory danych json w postaci STATYCZNYCH zmiennych lokalnych autorejestrowanych w konstruktorach
 #include "fasada.hpp"
 #include "processor_get.h"
 #include "processor_set.h"
@@ -18,20 +18,20 @@ namespace fasada
 
 void register_processors(bool WithWriters)
 {
-    static fasada::processor_get  GET;//Istnieje co najmniej jedna zmienna kazdego typu
-    static fasada::processor_dfs  DFS;//--//---
-    static fasada::processor_ls   LST; //--//---
-    static fasada::processor_find FIND;//--//---
-    static fasada::processor_check_file CHECK;//--//---
+    static fasada::processor_get      GET;//Istnieje co najmniej jedna zmienna każdego typu
+    static fasada::processor_dfs      DFS;//--//---
+    static fasada::processor_ls       LST; //--//---
+    static fasada::processor_find    FIND;//--//---
+    static fasada::processor_check_file CHECK_FILE;//--//---
 
-    static fasada::processor_ls  Def("default");//ale mozna robić aliasy
-    static fasada::processor_ls  Dir("dir");
-    static fasada::processor_ls  Lst("lst");
-    static fasada::processor_dfs Tree("tree");
+    static fasada::processor_ls       Def("default");//ale można też robić aliasy
+    static fasada::processor_ls       Dir("dir");
+    static fasada::processor_ls       Lst("lst");
+    static fasada::processor_dfs     Tree("tree");
 
     if(WithWriters)
     {
-        static fasada::processor_set  SET;//Istnieje co najmniej jedna zmienna kazdego typu
+        static fasada::processor_set  SET;//Istnieje co najmniej jedna zmienna każdego typu
         static fasada::processor_add  ADD;//--//---
         static fasada::processor_del  DEL;//--//---
         static fasada::processor_ren  REN;//--//---
@@ -39,9 +39,9 @@ void register_processors(bool WithWriters)
         static fasada::loader_csv     CSV;//--//---
         static fasada::loader_json   JSON;//--//---
                                                                   //ale mozna robić aliasy
-        static fasada::loader_csv      LinkedInCsv("LinkedInCsv");
-        //static fasada::loader_json   TwitterJson("TwitterJson");//Json'y z Twittera mają takie cosie na początku "window.YTD.tweet.part0 ="
-                                                                  //które wywalają boostowy parser
+        static fasada::loader_csv    LinkedInCsv("LinkedInCsv");
+        static fasada::loader_json   TwitterJson("TwitterJson");//Json'y z Twittera mają takie cosie na początku "window.YTD.tweet.part0 ="
+                                                                //które wywalają boostowy parser, ale już je usuwamy i wpisujemy jako atrybut _lead
     }
 }
 
