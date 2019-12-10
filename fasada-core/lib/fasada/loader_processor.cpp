80e14b8c8d967b6702190fb781975d5069952892
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-12-11 00:22:36 +0100

    First useable version of loader_processor base class

--- fasada-core/lib/fasada/loader_processor.cpp
+++ fasada-core/lib/fasada/loader_processor.cpp
@@ -26,25 +26,37 @@ loader_processor::~loader_processor()
 
 //default HTML form for this processor
 std::string loader_processor::Form=
-        ""
+        "<form action=\"${fullpath}!${proc}\" class=\"fasada_form\">"
+        "\n<input name=\"html\"   type=\"hidden\" >"
+        "\n<input name=\"long\"   type=\"hidden\" >"
+        "\n<input name=\"force\"  type=\"hidden\"  value=\"true\" >"
+        "\n<input name=\"ready\"  type=\"hidden\"  value=\"true\" >" ///value=\"$is_ready\" >"
+        "\n<BR><H3>REPLACE ALL CURRENT DATA BELOW ${fullpath} WITH DATA FROM FILE?</H3>"
+        ///"   <input name=\"subpath\" type=\"$input_of_subpath\"   size=\"$size_of_subpath\"   value=\"$subpath\">"
+        ///"\n<BR>..."
+        "\n<BR><input type=\"submit\" value=\"YES\" >"
+        "\n&nbsp;<input type=\"button\" value=\"CANCEL\" onclick=\"window.history.back();\" >"
+        "\n&nbsp;<a class=\"fasada_action\" href=\"${fullpath}?ls&html&long\" >LSL</A>&nbsp;&nbsp; "
+        "\n&nbsp;<a class=\"fasada_action\" href=\"${fullpath}?dfs&html&long\" >TREE</A>&nbsp;&nbsp; \n"
+        "</form>";
         ;
-
-std::string replace_all_variables(std::string template_version,URLparser& request)
-{
-    //MODYFIKACJE ZMIENNYMI Z REQUEST
-    //...
-    return template_version;//Już zmodyfikowana
-}
 
 void loader_processor::_implement_read(ShmString& o,const pt::ptree& top,URLparser& request)   //TODO
 //Wspólna funkcja formularza dla wszystkich loaderów
 {
+    bool        html=request.asHTML();
+    if(!html)
+        throw(tree_processor_exception("PTREE PROCESSOR "+procName+" CANNOT PREPARE FORM OTHER THAN HTML!"));
+
+    o+=ipc::string(EXT_PRE)+"htm\n";
+    o+=getHtmlHeaderDefaults(request.getFullPath())+"\n";
     o+=replace_all_variables(Form,request);
+
+    o+=getHtmlClosure();
     //throw(tree_processor_exception("PTREE LOADER PROCESSOR "+procName+" IS NOT IMPLEMENTED AS A READER!"));
 //See: https://www.boost.org/doc/libs/1_32_0/libs/filesystem/doc/operations.htm#last_write_time
 //OR   https://en.cppreference.com/w/cpp/filesystem/last_write_time  
 }
-
 
 void loader_processor::_implement_write(ShmString& o,pt::ptree& top,URLparser& request)
 {
