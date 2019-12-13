c7f9666709f54ab81a254473317beb82cc82fadd
Wojciech T. Borkowski <wborkowsk@gmail.com>
2019-12-13 15:53:10 +0100

    parameter 'suggestion' introduced to processor_find

--- fasada-core/lib/fasada/processor_find.cpp
+++ fasada-core/lib/fasada/processor_find.cpp
@@ -33,6 +33,7 @@ std::string processor_find::Form=
         "\n<input name=\"html\"   type=\"hidden\" >"
         "\n<input name=\"long\"   type=\"hidden\" >"
         "\n<input name=\"ready\"   type=\"hidden\"   value=\"$is_ready\" >"
+        "\n&#x1F50D; &#x1F50D; &#x1F50D; &#x1F50D; &#x1F50D; &#x1F50D;"
         "\n<BR>SUBPATH:     "
         "   <input name=\"subpath\" type=\"$input_of_subpath\"   size=\"$size_of_subpath\"   value=\"$subpath\">"
         "\n<BR>FIELD NAME: "
@@ -40,7 +41,8 @@ std::string processor_find::Form=
         "\n<BR>FIELD VALUE: "
         "   <input name=\"value\"   type=\"$input_of_value\"   size=\"$size_of_value\"   value=\"$value\">"
         "\n<BR>WILL BE FIND IN <B class=fasada_path>'$path'</B> "
-        "   <input type=\"submit\" value=\"OK\">"
+        "\n&nbsp;<input type=\"submit\" value=\"OK\">"
+        "\n&nbsp;<input type=\"button\" value=\"CANCEL\" onclick=\"window.history.back();\" >"
         "\n<BR><a class=\"fasada_action\" href=\"${fullpath}?ls&html&long\">LSL</A>&nbsp;&nbsp; "
         "\n<a class=\"fasada_action\" href=\"${fullpath}?dfs&html&long\">TREE</A>&nbsp;&nbsp; \n" ///TODO Not in the form!
         "</form>";
@@ -78,54 +80,65 @@ void processor_find::_implement_read(ShmString& o,const pt::ptree& top,URLparser
             boost::replace_all(ReadyForm,"$is_ready","true");
         }
 
-        ///<input name="subpath" type="$input_of_subpath"  value="$subpath" size="$size_of_subpath">
-        if( request.find("subpath") == request.end() || request["subpath"]=="" )
+        if( request.find("sugestion") != request.end())//This is valid only at the begining of setting search!
         {
-            boost::replace_all(ReadyForm,"$input_of_subpath","text");
-            boost::replace_all(ReadyForm,"$subpath",STR_DEFAULT_FILTER);
-            boost::replace_all(ReadyForm,"$size_of_subpath",STR_DEFAULT_LEN_OF_SUBPATH);
+            auto Sugestion=request["sugestion"];
+            if(request.find("field") ==  request.end()  )
+                boost::replace_all(ReadyForm,"$value",Sugestion);
+            if(request.find("value") ==  request.end()  )
+                boost::replace_all(ReadyForm,"$field",Sugestion);
         }
-        else
+        //else ???
         {
-            boost::replace_all(ReadyForm,"$input_of_subpath","hidden");
-            std::string replacer=(request["subpath"]+"\"><I class=\"fasada_val\">"+request["subpath"]+"</I>");
-            boost::replace_all(ReadyForm,"$subpath\">",replacer);
-            boost::replace_all(ReadyForm,"$size_of_subpath","1");        boost::replace_all(ReadyForm,"$proc",procName);
-            boost::replace_all(ReadyForm,"$fullpath",fullpath);
-        }
+            ///<input name="subpath" type="$input_of_subpath"  value="$subpath" size="$size_of_subpath">
+            if( request.find("subpath") == request.end() || request["subpath"]=="" )
+            {
+                boost::replace_all(ReadyForm,"$input_of_subpath","text");
+                boost::replace_all(ReadyForm,"$subpath",STR_DEFAULT_FILTER);
+                boost::replace_all(ReadyForm,"$size_of_subpath",STR_DEFAULT_LEN_OF_SUBPATH);
+            }
+            else
+            {
+                boost::replace_all(ReadyForm,"$input_of_subpath","hidden");
+                std::string replacer=(request["subpath"]+"\"><I class=\"fasada_val\">"+request["subpath"]+"</I>");
+                boost::replace_all(ReadyForm,"$subpath\">",replacer);
+                boost::replace_all(ReadyForm,"$size_of_subpath","1");        boost::replace_all(ReadyForm,"$proc",procName);
+                boost::replace_all(ReadyForm,"$fullpath",fullpath);
+            }
 
-        ///<input name="field"   type="$input_of_field"    value="$field"   size="$size_of_field">
-        if( request.find("field") == request.end() || request["field"]=="" )
-        {
-            boost::replace_all(ReadyForm,"$input_of_field","text");
-            boost::replace_all(ReadyForm,"$field",STR_DEFAULT_FILTER);
-            boost::replace_all(ReadyForm,"$size_of_field",STR_DEFAULT_LEN_OF_NAME);
-        }
-        else
-        {
-            boost::replace_all(ReadyForm,"$input_of_field","hidden");
-            std::string replacer=(request["field"]+"\"><I class=\"fasada_val\">"+request["field"]+"</I>");
-            boost::replace_all(ReadyForm,"$field\">",replacer);
-            boost::replace_all(ReadyForm,"$size_of_field","1");
-        }
+            ///<input name="field"   type="$input_of_field"    value="$field"   size="$size_of_field">
+            if( request.find("field") == request.end() || request["field"]=="" )
+            {
+                boost::replace_all(ReadyForm,"$input_of_field","text");
+                boost::replace_all(ReadyForm,"$field",STR_DEFAULT_FILTER);
+                boost::replace_all(ReadyForm,"$size_of_field",STR_DEFAULT_LEN_OF_NAME);
+            }
+            else
+            {
+                boost::replace_all(ReadyForm,"$input_of_field","hidden");
+                std::string replacer=(request["field"]+"\"><I class=\"fasada_val\">"+request["field"]+"</I>");
+                boost::replace_all(ReadyForm,"$field\">",replacer);
+                boost::replace_all(ReadyForm,"$size_of_field","1");
+            }
 
-        ///<input name="value"   type="$input_of_value"    value="$value"   size="$size_of_value">"
-        if( request.find("value") == request.end() || request["value"]=="" )
-        {
-            boost::replace_all(ReadyForm,"$input_of_value","text");
-            boost::replace_all(ReadyForm,"$value",STR_DEFAULT_FILTER);
-            boost::replace_all(ReadyForm,"$size_of_value","24");
-        }
-        else
-        {
-            boost::replace_all(ReadyForm,"$input_of_value","hidden");
-            std::string replacer=(request["value"]+"\"><I class=\"fasada_val\">"+request["value"]+"</I>");
-            boost::replace_all(ReadyForm,"$value\">",replacer);
-            boost::replace_all(ReadyForm,"$size_of_value","1");
+            ///<input name="value"   type="$input_of_value"    value="$value"   size="$size_of_value">"
+            if( request.find("value") == request.end() || request["value"]=="" )
+            {
+                boost::replace_all(ReadyForm,"$input_of_value","text");
+                boost::replace_all(ReadyForm,"$value",STR_DEFAULT_FILTER);
+                boost::replace_all(ReadyForm,"$size_of_value","24");
+            }
+            else
+            {
+                boost::replace_all(ReadyForm,"$input_of_value","hidden");
+                std::string replacer=(request["value"]+"\"><I class=\"fasada_val\">"+request["value"]+"</I>");
+                boost::replace_all(ReadyForm,"$value\">",replacer);
+                boost::replace_all(ReadyForm,"$size_of_value","1");
+            }
         }
 
         o+=ReadyForm;
-        o+=getHtmlClosure();
+        o+=getHtmlClosure(_compiled);
     }
 }
 
@@ -242,7 +255,7 @@ void processor_find::_implement_substring_find(ShmString& o,const pt::ptree& top
         o+="</UL>";
         o+=boost::lexical_cast<val_string>(counter)+"<BR>\n";
         if(counter>10) _implement_action_panel(o,request);
-        o+="\n"+getHtmlClosure();
+        o+="\n"+getHtmlClosure(_compiled);
     }
 }
 
